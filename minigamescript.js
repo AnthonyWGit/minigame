fef7cf6d17eac9fe11cfed1bcbdea4e079b238e1
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-03-31 16:47:17 +0200

    Added commentaries, we shuffl'in

--- minigamescript.js
+++ minigamescript.js
@@ -4,17 +4,26 @@ box.classList.add("box"); //we manipulate the list of class of constant box (whi
 const board = document.querySelector("#board") /*Declaration of a box constant returning the first element that
                                                      is descendant of a node matching id #board selector*/
 board.appendChild(box) // Every node matching #board id has a child called constant box now. (The one above !)
-box.innerText = "One" //String "One" is added in our <div class="box"></div> box
+box.innerText = 1 //int 1 is added in our <div class="box"></div> box
 for (let i = 1; i <= 10; i++)       //For i starting at 1, excute the code in brackets until i reaches 10, add 1 each time ALL of the instructions in the brackets
                                     // are done 
 {
-    let newbox = box.cloneNode()        //Create a newbox variable and give a clone of the node box.
+    const newbox = box.cloneNode()        //Create a newbox variable and give a clone of the node box.
     newbox.innerText = i                //Inside the newbox variable looking like <div class ="newbox"></div> put i between tags
     board.appendChild(newbox)           //To display the new boxes we have to appen the new node newbox to the parent node board
 }                                   // 
-for (let i = 1; i <= 10; i++)
+/*for (let i = 1; i <= 10; i++)
 {
     let newbox = box.cloneNode()
     newbox.innerText = i
     board.appendChild(box)
+}
+*/
+let i = board.children.length, k, temp  //children are returned in k, lenght in temp
+while(--i > 0) // Each time all the code in scope is executed, retire 1 to i, code in scope is executed until i reaches 0
+{
+k = Math.floor(Math.random() * (i+1)) // store in k the greatest int less than or equal to a random number between 0 and 1 times i+1
+temp = board.children[k] // temp gives k postition in board
+board.children[k] = board.children[i] //k position in board is replaced by i position in the same board
+board.appendChild(temp) // Node temp is added to board 
 }
