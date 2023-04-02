2f3ef96094bdc475799e703c060e91597ef91368
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-04-02 14:44:30 +0200

    correctedTypoInFunction,SquaresValidatedInOrder

--- minigamescript.js
+++ minigamescript.js
@@ -1,14 +1,14 @@
 
 function shuffleChildren(parent)
 {
-    let children =parent.children //Children becomes the infant of parent paramater given above
-    let i = children.lenght, k, temp //Variables i, k and temp are declared. i takes the value of the number of objects of children in all board const
+    let children = parent.children //Children becomes the infant of parent paramater given above
+    let i = children.length, k, temp //Variables i, k and temp are declared. i takes the value of the number of objects of children in all board const
     while(--i > 0) // Each time all the code in scope is executed, retire 1 to i, code in scope is executed until i reaches 0
     {
         k = Math.floor(Math.random() * (i+1)) // store in k the greatest int less than or equal to a random number between 0 and 1 times i+1
-        temp = board.children[k] // temp gives k postition in board
-        board.children[k] = board.children[i] //k position in board is replaced by i position in the same board
-        board.appendChild(temp) // Node temp is added to board 
+        temp = children[k] // temp gives k postition in board
+        children[k] = children[i] //k position in board is replaced by i position in the same board
+        parent.appendChild(temp) // Node temp is added to board 
     }    
 }
 
@@ -17,8 +17,11 @@ box.classList.add("box"); //we manipulate the list of class of constant box (whi
 
 const board = document.querySelector("#board") /*Declaration of a box constant returning the first element that
                                                      is descendant of a node matching id #board selector*/
-board.appendChild(box) // Every node matching #board id has a child called constant box now. (The one above !)
-box.innerText = 1 //int 1 is added in our <div class="box"></div> box
+
+let nb = 1
+
+//board.appendChild(box) // Every node matching #board id has a child called constant box now. (The one above !)
+//box.innerText = 1 //int 1 is added in our <div class="box"></div> box
 for (let i = 1; i <= 10; i++)       //For i starting at 1, excute the code in brackets until i reaches 10, add 1 each time ALL of the instructions in the brackets
                                     // are done 
 {
@@ -27,8 +30,13 @@ for (let i = 1; i <= 10; i++)       //For i starting at 1, excute the code in br
     board.appendChild(newbox)           //To display the new boxes we have to appen the new node newbox to the parent node board
     newbox.addEventListener("click",function() //to each newbox on the board add an EvenListener that will call the unnamed function above each time newbox is clicked
     {
+        if (i == nb) //The player has to click the number 1 first, then 2, etc.
+        {
         console.log("Boîte n°"+i+",  click !") //Display "boite n°i, click !" in console
-        newbox.classList.add("box-valid") //add the class "box-valid" when clicked
+        newbox.classList.add("box-valid") //add the class "box-valid" when clicked            
+        nb++
+        }
+
     })
 }                                   // 
 /*for (let i = 1; i <= 10; i++)
@@ -38,5 +46,5 @@ for (let i = 1; i <= 10; i++)       //For i starting at 1, excute the code in br
     board.appendChild(box)                  *******
 }
 */
-
 shuffleChildren(board)  //function is called to display to board and so all the elements in it will appear as well
+
